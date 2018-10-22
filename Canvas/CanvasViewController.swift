1a1a124a27ba0a027bd2bcc2ded5232b1d172f6d
Claudia Nelson <claudiaareneee@gmail.com>
2018-10-21 20:36:31 -0500

    tray view pan gesture

---
+++ Canvas/CanvasViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CanvasViewController.swift
+//  Canvas
+//
+//  Created by Claudia Nelson on 10/20/18.
+//  Copyright © 2018 claudia. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+    @IBOutlet weak var trayView: UIView!
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        trayDownOffset = 160
+        trayUp = trayView.center // The initial position of the tray
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset) // The position of the tray transposed down
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        let location = sender.location(in: view)
+        let translation = sender.translation(in: view)
+        let velocity = sender.velocity(in: view)
+        
+        if sender.state == .began {
+            trayOriginalCenter = trayView.center
+            print("Gesture began")
+        } else if sender.state == .changed {
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+            print("Gesture is changing")
+        } else if sender.state == .ended {
+            if velocity.y > 0 {
+                UIView.animate(withDuration: 0.3) {
+                    self.trayView.center = self.trayDown
+                }
+            } else {
+                UIView.animate(withDuration: 0.3) {
+                    self.trayView.center = self.trayUp
+                }
+            }
+            print("Gesture ended")
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
